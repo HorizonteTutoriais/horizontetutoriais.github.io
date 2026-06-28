90c3ff6adca97417a32d8fc21fd08fd210f91d98
HorizonteTutoriais <drchorizonti@gmail>
2026-06-28 11:03:44 -0300

    Nova postagem, Horizon Tela Ligada 1

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,8 +8,8 @@ const items = [
     "data": "2026-06-28",
     "download": "https://4br.me/BaixarHorizonTelaLigada1",
     "downloads": null,
-    "guia_link": "https://4br.me/BaixarGuiaHorizonTela",
-    "tutorial": "#",
+    "guia_link": "https://4br.me/BaixarGuiaHorizonTela5",
+    "tutorial": "https://www.youtube.com/embed/WhXIGWKviSo?autoplay=1&loop=1&playlist=WhXIGWKviSo",
     "especificacoes": {
         "versao": "1.0",
         "tamanho": "7.03M",
