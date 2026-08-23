42a33ae5c62f1321f493226816ade54e09009baf
HorizonteTutoriais <drchorizonti@gmail>
2026-08-22 21:29:49 -0300

    Atualização do HorizonTV Cine 1

--- assets/js/data.js
+++ assets/js/data.js
@@ -11,7 +11,7 @@ const items = [
     "guia_link": "https://www.mediafire.com/file/zmi7evh9cir76ig/Guia.md/file",
     "tutorial": "#",
     "especificacoes": {
-        "versao": "1.1",
+        "versao": "1.0",
         "tamanho": "65.52M",
         "android": "5.0",
         "desenvolvedora": "Horizonte Tutoriais"
@@ -39,7 +39,7 @@ const items = [
     "guia_link": "https://www.mediafire.com/file/zmi7evh9cir76ig/Guia.md/file",
     "tutorial": "#",
     "especificacoes": {
-        "versao": "2.1",
+        "versao": "2.0",
         "tamanho": "39.69M",
         "android": "5.0",
         "desenvolvedora": "Horizonte Tutoriais"
