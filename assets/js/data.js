cda0b4b03444789ad20e5f30bfb075ef935570af
HorizonteTutoriais <drchorizonti@gmail>
2026-08-24 22:29:19 -0300

    Adicionar botoes Guia e Tutorial na pagina tecnica

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,14 +4,14 @@ const items = [
     "nome": "📺🎬 Horizon TV Cine 2",
     "tipo": "Aplicativo",
     "descricao": "em breve",
-    "imagem": "https://i.postimg.cc/WbJtSXVD/In-Shot-20260821-033338755.png\",",
+    "imagem": "https://i.postimg.cc/WbJtSXVD/In-Shot-20260821-033338755.png",
     "data": "2026-08-23",
     "download": "https://www.mediafire.com/file/839vhk6lf6zvdg7/Horizon-TV-Cine-2-portugues-completo-interface.apk/file",
     "downloads": null,
     "guia_link": "https://www.mediafire.com/file/zmi7evh9cir76ig/Guia.md/file",
     "tutorial": "#",
     "especificacoes": {
-        "versao": "2.1",
+        "versao": "2.0",
         "tamanho": "39.69M",
         "android": "5.0",
         "desenvolvedora": "Horizonte Tutoriais"
